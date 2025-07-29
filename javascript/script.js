03d99122e2fc39d936249e3e03db9152b71c91d7
“code-hopper” <ameykhondekar01@gmail.com>
2025-07-29 19:17:48 +0530

    javascript lc1

---
+++ javascript/script.js
@@ -0,0 +1,59 @@
+console.log("this is written usign external js file !")
+
+console.log(document.querySelector("h1"))
+
+// syntax to create a variable
+
+// scope(var,let,const) variable_name assignment-oprator(=) data/value();
+
+// scope : var (public), let, const (protected)
+
+// let/const laptop
+
+// const adhar_number 
+
+// let balance
+
+// datatype 
+
+// primitive 
+// number(int, float, short, long, byte, double), string, boolean
+
+// non-primtive
+// object, array, function, null, NaN
+
+// int - -number to +number (no decimal point)
+
+// "some name" - string
+
+// boolean - true/false
+
+// rules to create a variable_name
+
+// 1. variable can be alpha-numeric but cannot start with a number.
+// box1 right
+// 1box wrong
+// 2. variable has to be unique (in a the same scope) and no space in between.
+
+// 3. no special chachters expection " _ "
+
+// {
+
+//     let data_1 = 10
+
+// }
+
+// {
+//     let data = "name"
+// }
+
+// let 1box invalid
+// let 1 box invalid
+// let box1 valid
+// let box 1 invalid
+// let box#1 invalid
+// let box_1 valid
+
+// let age = 22;
+
+// console.log("hello ! name your age is " + age)
