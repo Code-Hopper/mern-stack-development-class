96518012cb93c200abc881cd45784256974a77e6
“code-hopper” <ameykhondekar01@gmail.com>
2025-08-02 19:31:05 +0530

    conditional statments

--- javascript/lessons/1.oprators/script.js
+++ javascript/lessons/1.oprators/script.js
@@ -21,3 +21,12 @@
 // -> .
 
 // to create 2 variables and perform airthmatic oprations on them
+
+let num1 = 10
+let num2 = 20
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 / num2)
+console.log(num1 * num2)
+console.log(num1 % num2)
