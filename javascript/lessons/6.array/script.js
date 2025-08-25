a4e361dfc68614ff4ae475425eea2e873d22c93b
“code-hopper” <ameykhondekar01@gmail.com>
2025-08-25 19:18:29 +0530

    array of array and objects

--- javascript/lessons/6.array/script.js
+++ javascript/lessons/6.array/script.js
@@ -1,139 +1,154 @@
-// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// // let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// // console.log(numbers[0])
-// // console.log(numbers[1])
-// // console.log(numbers[2])
-// // console.log(numbers[3])
-// // console.log(numbers[4])
-// // console.log(numbers[5])
-// // console.log(numbers[6])
-// // console.log(numbers[numbers.length - 1])
+// // // console.log(numbers[0])
+// // // console.log(numbers[1])
+// // // console.log(numbers[2])
+// // // console.log(numbers[3])
+// // // console.log(numbers[4])
+// // // console.log(numbers[5])
+// // // console.log(numbers[6])
+// // // console.log(numbers[numbers.length - 1])
 
-// // numbers[0] = 1000
+// // // numbers[0] = 1000
 
-// // console.log(numbers[0])
+// // // console.log(numbers[0])
 
-// // for(let index = 0 ; index < numbers.length ; index++){
-// //     console.log(`terverse data : ${numbers[index]}`)
+// // // for(let index = 0 ; index < numbers.length ; index++){
+// // //     console.log(`terverse data : ${numbers[index]}`)
+// // // }
+
+// // // Array index out of bound
+
+// // // let index = 0
+
+// // // while (index < numbers.length) {
+// // //     console.log(`terverse data : ${numbers[index]}`)
+
+// // //     index++
+// // // }
+
+// // // for of
+// // // for(let element of array){
+// // //     console.log(element)
+// // // }
+
+// // // let names = ["amey", "sahil", "rohit", "om"]
+
+// // // for (let number of numbers) {
+// // //     console.log(number)
+// // // }
+
+// // // for(let name of names){
+// // //     console.log(name)
+// // // }
+
+// // // forEach()
+
+// // // numbers.forEach((x,i)=>{
+// // //     console.log(`the element at index ${i} is ${x}`)
+// // // })
+
+// // // map
+
+// // function square(number) {
+// //     return number * number
 // // }
 
-// // Array index out of bound
+// // // let data = numbers.forEach((number) => {
+// // //     console.log(square(number))
+// // // })
 
-// // let index = 0
 
-// // while (index < numbers.length) {
-// //     console.log(`terverse data : ${numbers[index]}`)
+// // let data = numbers.map(square)
 
-// //     index++
-// // }
+// // console.log(data)
 
-// // for of
-// // for(let element of array){
-// //     console.log(element)
-// // }
+// // console.log([1,2,3,4].length)
 
-// // let names = ["amey", "sahil", "rohit", "om"]
+// // updating
 
-// // for (let number of numbers) {
-// //     console.log(number)
-// // }
+// // to add a new element within the array
+// // Array.push(element) , Array.unshift(element)
 
-// // for(let name of names){
-// //     console.log(name)
-// // }
+// // let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// // forEach()
+// // arr.push(5)
+// // arr.push(6)
+// // arr.push(7)
 
-// // numbers.forEach((x,i)=>{
-// //     console.log(`the element at index ${i} is ${x}`)
+// // arr.unshift(8)
+// // arr.unshift(9)
+// // arr.unshift(10)
+
+// // console.log(arr)
+
+// // // to remove element from array 
+// // // Array.pop(), arr.shift()
+
+// // console.log(arr.pop())
+// // console.log(arr.pop())
+// // console.log(arr.pop())
+// // console.log(arr.pop())
+// // console.log(arr.shift())
+// // console.log(arr.shift())
+// // console.log(arr.shift())
+
+// // console.log(arr)
+
+// // index wise
+// // Array.splice(start index, delete count,replacement(optional))
+
+// // arr.splice(2,0,10,"hello")
+
+// // console.log(arr)
+
+// // arr.slice(), arr.indexOf(), arr.lastIndexOf()
+
+// // Array.includes(element)
+
+// // console.log(arr.includes(10))
+
+// // let evenData = arr.filter((element, index) => {
+// //     return element % 2 == 0
 // // })
 
-// // map
+// // console.log(evenData)
 
-// function square(number) {
-//     return number * number
-// }
+// // console.log(arr)
 
-// // let data = numbers.forEach((number) => {
-// //     console.log(square(number))
-// // })
+// let numbers = [3, 1, 2, 5, 4]
 
+// numbers.sort((a, b) => { return b - a })
 
-// let data = numbers.map(square)
+// console.log(numbers)
 
-// console.log(data)
+// let names = ["b", "d", "a", "c", "A"]
 
-// console.log([1,2,3,4].length)
+// // names.sort()
 
-// updating
-
-// to add a new element within the array
-// Array.push(element) , Array.unshift(element)
-
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-// arr.push(5)
-// arr.push(6)
-// arr.push(7)
-
-// arr.unshift(8)
-// arr.unshift(9)
-// arr.unshift(10)
-
-// console.log(arr)
-
-// // to remove element from array 
-// // Array.pop(), arr.shift()
-
-// console.log(arr.pop())
-// console.log(arr.pop())
-// console.log(arr.pop())
-// console.log(arr.pop())
-// console.log(arr.shift())
-// console.log(arr.shift())
-// console.log(arr.shift())
-
-// console.log(arr)
-
-// index wise
-// Array.splice(start index, delete count,replacement(optional))
-
-// arr.splice(2,0,10,"hello")
-
-// console.log(arr)
-
-// arr.slice(), arr.indexOf(), arr.lastIndexOf()
-
-// Array.includes(element)
-
-// console.log(arr.includes(10))
-
-// let evenData = arr.filter((element, index) => {
-//     return element % 2 == 0
+// names.sort((a, b) => {
+//     if (a > b) {
+//         return 1
+//     } else {
+//         return -1
+//     }
 // })
 
-// console.log(evenData)
+// console.log(names)
 
-// console.log(arr)
+// // search[linear, binary], sort[bubble, selection]
 
-let numbers = [3, 1, 2, 5, 4]
+let numbers = [1,2,3,4,5,6,7,9,10] 
 
-numbers.sort((a, b) => { return b - a })
+// numbers.length -> how many elements are present in the array
 
-console.log(numbers)
+// for(let index = 0 ; index < numbers.length ; index++){
+//     console.log(numbers[index])
+// }
 
-let names = ["b", "d", "a", "c", "A"]
+// for of
 
-// names.sort()
-
-names.sort((a, b) => {
-    if (a > b) {
-        return 1
-    } else {
-        return -1
-    }
-})
-
-console.log(names)
-
-// search[linear, binary], sort[bubble, selection]
+for(let element of numbers){
+    console.log(element)
+}
+// 1....10
---
+++ javascript/lessons/7.object/script.js
@@ -0,0 +1,99 @@
+// key:value pair
+
+let student1 = {
+    name: "Amey Khondekar",
+    phone: 9766696550,
+    address: "Nagpur",
+    subjects: ["Math", "Java"]
+}
+
+
+let student2 = {
+    name: "Rohit Kumar",
+    phone: 8857032120,
+    address: "Pune",
+    subjects: ["Science", "Java"],
+    action: () => { alert("hi !") }
+}
+
+// object.key
+
+// console.log(student1.name) //Amey Khondekar
+// console.log(student1.phone) //9766696550
+// console.log(student1.address) //Nagpur
+// console.log(student1.subjects) //array
+
+// console.log(student2.name) //Rohit Kumar
+// console.log(student2.phone) //8857032120
+// console.log(student2.address) //Pune
+// console.log(student2.subjects) //array
+
+// student2.action()
+
+let students = [
+    student1, student2,
+    { name: "Sahil Kumar", phone: 907582864, address: "mumbai", subjects: ["English", "C++"] }
+]
+
+// Array of Object
+
+// for(let student of students){
+//     console.log(student.name)
+// }
+
+// for (let key in student1) {
+//     console.log(key + " : " + student1[key])
+// }
+
+// // destructuring of array and objects
+
+// let array = [1, 2, 3, 4]
+// let object = { key1: "value1", key2: "value2" }
+
+// let [a, b, c, d] = array
+
+// console.log(a)
+// console.log(d)
+
+// console.log(array)
+// console.log(...array)
+
+// let numbers = [5, 6, 7, ...array]
+
+// console.log(numbers)
+
+// let { key1 } = object
+
+// console.log(key1)
+
+// let { name } = student1 
+
+let response = {
+    title: "title",
+    status: true,
+    output: [5, 4, 6]
+}
+
+
+function checkTask(response) {
+    let { status, output } = response
+
+    if (status) {
+        console.log("checking output")
+
+        let [op1, op2, op3] = output
+
+        if (op1 == 1 && op2 == 2 && op3 == 3) {
+            // console.log("you have completed the task successfully !")
+            return true
+        } else {
+            // console.log("you have completed the task unsuccessfully !")
+            return false
+        }
+    } else {
+        // console.log('task not completed !')
+        return false
+    }
+}
+
+console.log(checkTask(response))
