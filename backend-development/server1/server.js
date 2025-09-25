34593116fd53d348ed53fdf2351b4c838e8e5cf2
“code-hopper” <ameykhondekar01@gmail.com>
2025-09-25 09:22:56 +0530

    import export statements and route

--- backend-development/server1/server.js
+++ backend-development/server1/server.js
@@ -7,13 +7,33 @@
 
 import express from "express"
 
+// import default
+
+import something from "./data.js"
+
+// import named
+
+import { data2, doSomething2, doSomething3, someMethod } from "./data.js"
+
 const app = express()
 
 let port = 5010
 
 app.get("/", (req, res) => {
-    res.send("welcome to our home route")
+    res.status(200).json({ message: "welcome to our home route" })
 })
+
+console.log("server value for import is : ", something)
+
+something()
+
+console.log(data2)
+
+doSomething2()
+
+doSomething3()
+
+someMethod()
 
 app.listen(port, () => {
     console.log(`server is running on port ${port} !`)
@@ -21,3 +41,11 @@ app.listen(port, () => {
 
 // http://localhost:5010
 // welcome to our home route
+
+// query, path params, form data, raw JSON
+
+// whenever user send data using either of above formats we will be able to read that data from req(request object) within the http method/s
+
+// send, sendFile, render(view engine), json
+
+// Model(Schema, Routers, Utils) View(UI,View Engine,React) Controllers
