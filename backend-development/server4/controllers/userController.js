e750e97578249c3e01821fa8d2af72ac76111e3f
“code-hopper” <ameykhondekar01@gmail.com>
2025-11-04 09:21:21 +0530

    nodemailer

--- backend-development/server4/controllers/userController.js
+++ backend-development/server4/controllers/userController.js
@@ -1,5 +1,47 @@
 import { userModel } from "../models/userSchema.js"
 
+import nodemailer from "nodemailer"
+
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.USER_EMAIL,
+        pass: process.env.USER_EMAIL_PASSWORD,
+    },
+});
+
+let genrateOTP = () => {
+    return Math.floor((Math.random() * 9000) + 1000)
+}
+
+let sendEmail = async (email) => {
+    try {
+
+        let otp = genrateOTP()
+
+        let result = await transporter.sendMail({
+            from: process.env.USER_EMAIL,
+            to: email,
+            subject: "test otp email !",
+            html: `
+                <h1>your otp is <span style="color:red;">${otp}</span> </h1>
+            `
+        })
+        console.log(result)
+
+        console.log("an email has been sent !")
+
+        return result
+
+    } catch (err) {
+        console.log("unable to send email: ", err)
+        throw err
+    }
+}
+
 let postHandleUserRegister = async (req, res) => {
     try {
 
@@ -14,6 +56,8 @@ let postHandleUserRegister = async (req, res) => {
         let emailObject = {
             userEmail: email, verified: false
         }
+
+        await sendEmail(email)
 
         let newUser = new userModel({ name, phone, email: emailObject, address, password })
 
@@ -32,5 +76,7 @@ let postHandleUserRegister = async (req, res) => {
 export { postHandleUserRegister }
 
 // send an otp to the email.userEmail (nodemailer)
+
 // verify that otp using a seprate route
+
 // if varified then update email.verified to be true
