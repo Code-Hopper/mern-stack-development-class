5f65e27e13ba61e317b26368b5c6dc05091cb624
“code-hopper” <ameykhondekar01@gmail.com>
2026-03-25 12:15:00 +0530

    Backdated commit message

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,62 +1,172 @@
-// console.log("hello javascript world !"); 
-// document.write("something !");
-// // window.alert("hello !")
+// // console.log("hello javascript world !"); 
+// // document.write("something !");
+// // // window.alert("hello !")
 
-// let n1 = 100
-// let n2 = 25
+// // let n1 = 100
+// // let n2 = 25
 
-// let sum = (2*(n1 + n2))/3
-// let sub = n1 - n2
-// let div = n1 / n2
-// let mul = n1 * n2
-// let mod = n1 % n2
+// // let sum = (2*(n1 + n2))/3
+// // let sub = n1 - n2
+// // let div = n1 / n2
+// // let mul = n1 * n2
+// // let mod = n1 % n2
 
-// console.log(sum)
-// console.log(sub)
-// console.log(div)
-// console.log(mul)
-// console.log(mod)
+// // console.log(sum)
+// // console.log(sub)
+// // console.log(div)
+// // console.log(mul)
+// // console.log(mod)
 
-// let data = 100;
+// // let data = 100;
 
-// let result = typeof(data)
+// // let result = typeof(data)
 
-// console.log(typeof(data))
+// // console.log(typeof(data))
 
-// windows.prompt()
+// // windows.prompt()
 
-// let data = window.prompt("enter your name !") // will always returns a string
+// // let data = window.prompt("enter your name !") // will always returns a string
 
+// // console.log(data)
+// // console.log(typeof(data))
+
+// // "2"+ "2" = "22" -> concatenation
+
+// // type casting
+
+// // number -> string
+// // number -> boolean
+
+// // string -> number
+// // string -> boolean 
+
+// // boolean -> number
+// // boolean -> string
+
+// let fname = "amey"
+
+// let mname = " anil"
+
+// let lname = " khondekar"
+
+// console.log(fname + mname + lname)
+
+// // stirng + string -> string
+// // string + number -> string
+// // number + string -> string
+// // number + number -> number 
+
+// // "2" + "2" = "22"
+// // "2" + 2 = "22"
+// // 2 + "2" = "22"
+// // 2 + 2 = 4
+
+// let data = 100
+
+// // variale++(post)
+// // ++variale(pre)
+
+// let result = 100 + --data
+
+// console.log(result)
+
+// console.log(data--) // 201
+// console.log(data) // 202
+
+// data = "something"
+
+// // type casting
+
+// // number -> string
+// // number -> boolean
+
+// // string -> number
+// // string -> boolean 
+
+// // boolean -> number
+// // boolean -> string
+
+// let data = 1234
+
+// console.log(typeof (data))
 // console.log(data)
-// console.log(typeof(data))
 
-// "2"+ "2" = "22" -> concatenation
+// // number -> string
 
-// type casting
+// // data = String(data)
+// // data = data.toString()
 
-// number -> string
-// number -> boolean
+// console.log(typeof (data))
+// console.log(data)
 
-// string -> number
-// string -> boolean 
+// let data = 1234
 
-// boolean -> number
+// console.log(typeof (data))
+// console.log(data)
+
+// // number -> boolean(true/false)
+
+// data = Boolean(data)
+// // all value expect 0 is true for boolean
+
+// console.log(typeof (data))
+// console.log(data)
+
+// let data = "123.4567"
+
+// console.log(typeof (data))
+// console.log(data)
+
+// // string -> number
+
+// data = Number(data)
+
+// // data = parseInt(data)
+
+// // if a string has a charch it will be converted into NaN for Number data type 
+
+// console.log(typeof (data))
+// console.log(data)
+
+// let data = ""
+
+// console.log(typeof (data))
+// console.log(data)
+
+// // string -> boolean
+
+// data = Boolean(data)
+// // empty string is false else true
+
+// console.log(typeof (data))
+// console.log(data)
+
+// let data = true
+
+// console.log(typeof (data))
+// console.log(data)
+
+// // boolean -> number
+
+// data = Number(data)
+
+// // if true then 1
+// // if false then 0 
+
+// console.log(typeof (data))
+// console.log(data)
+
+let data = true
+
+console.log(typeof (data))
+console.log(data)
+
 // boolean -> string
 
-let fname = "amey"
+data = String(data)
 
-let mname = " anil"
+// if true then true
+// if false then false
 
-let lname = " khondekar"
-
-console.log(fname + mname + lname)
-
-// stirng + string -> string
-// string + number -> string
-// number + string -> string
-// number + number -> number 
-
-// "2" + "2" = "22"
-// "2" + 2 = "22"
-// 2 + "2" = "22"
-// 2 + 2 = 4
+console.log(typeof (data))
+console.log(data)
