21334860a7085d255394514d5b4518bf991b8d4e
“code-hopper” <ameykhondekar01@gmail.com>
2026-04-23 08:48:28 +0530

    update

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,3 +1,62 @@
-console.log("hello javascript world !"); 
-document.write("something !");
-// window.alert("hello !")
+// console.log("hello javascript world !"); 
+// document.write("something !");
+// // window.alert("hello !")
+
+// let n1 = 100
+// let n2 = 25
+
+// let sum = (2*(n1 + n2))/3
+// let sub = n1 - n2
+// let div = n1 / n2
+// let mul = n1 * n2
+// let mod = n1 % n2
+
+// console.log(sum)
+// console.log(sub)
+// console.log(div)
+// console.log(mul)
+// console.log(mod)
+
+// let data = 100;
+
+// let result = typeof(data)
+
+// console.log(typeof(data))
+
+// windows.prompt()
+
+// let data = window.prompt("enter your name !") // will always returns a string
+
+// console.log(data)
+// console.log(typeof(data))
+
+// "2"+ "2" = "22" -> concatenation
+
+// type casting
+
+// number -> string
+// number -> boolean
+
+// string -> number
+// string -> boolean 
+
+// boolean -> number
+// boolean -> string
+
+let fname = "amey"
+
+let mname = " anil"
+
+let lname = " khondekar"
+
+console.log(fname + mname + lname)
+
+// stirng + string -> string
+// string + number -> string
+// number + string -> string
+// number + number -> number 
+
+// "2" + "2" = "22"
+// "2" + 2 = "22"
+// 2 + "2" = "22"
+// 2 + 2 = 4
