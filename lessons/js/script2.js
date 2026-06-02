1c76088b5becc6fe92bf75a704ba0ccd690d81a0
“code-hopper” <ameykhondekar01@gmail.com>
2026-06-02 08:47:52 +0530

    exception

--- lessons/js/script2.js
+++ lessons/js/script2.js
@@ -35,24 +35,78 @@
 // setter & getter
 
 // we will have to create an object of date class to use date functions
-let date = new Date()
+// let date = new Date()
 
-date.setDate(1)
-date.setHours(0)
-date.setMinutes(5)
-date.setSeconds(14)
-date.setMilliseconds(1)
+// date.setDate(1)
+// date.setHours(0)
+// date.setMinutes(5)
+// date.setSeconds(14)
+// date.setMilliseconds(1)
 
-console.log(date.getTime()) // 1 jan 1970 00:00:00 to till date //  
-console.log(date.getFullYear())
-console.log(date.getMonth())
-console.log(date.getDate())
-console.log(date.getDay())
-console.log(date.getTimezoneOffset())
-console.log(date.getHours())
-console.log(date.getMinutes())
-console.log(date.getSeconds())
-console.log(date.getMilliseconds())
+// console.log(date.getTime()) // 1 jan 1970 00:00:00 to till date //  
+// console.log(date.getFullYear())
+// console.log(date.getMonth())
+// console.log(date.getDate())
+// console.log(date.getDay())
+// console.log(date.getTimezoneOffset())
+// console.log(date.getHours())
+// console.log(date.getMinutes())
+// console.log(date.getSeconds())
+// console.log(date.getMilliseconds())
 
-console.log(date.toLocaleDateString())
-console.log(date.toLocaleTimeString())
+// console.log(date.toLocaleDateString())
+// console.log(date.toLocaleTimeString())
+
+// try catch block -> expectional/error handling
+
+// line 1
+// line 2
+// line 3
+// try{
+//     line 4 - x[user input] 
+// }catch(err){
+//     // resolution
+// }
+// line 5
+// line 6
+
+// let number1 = 10
+// let number2 = 0
+
+// try {
+//     let division = number1 / number2;
+//     if (division == Infinity) {
+//         throw new Error("Division by 0 is not allowed !")
+//     }
+//     console.log(division)
+// } catch (err) {
+//     console.log(err)
+// } finally {
+//     console.log("code completed !")
+// }
+
+// try {
+//     console.log(a)
+// } catch (err) {
+//     console.log(err)
+// } finally {
+//     console.log("program completed !")
+// }
+
+// sync code{block code}
+// async code 
+
+// sync
+
+// lin1 -> 2s
+// lin2 -> 3s
+// lin3 -> 4s
+// lin4 -> 1s
+
+// 10s
+
+// // async 
+
+// line1 -> 2s | line2 -> 3 | line3 -> 4s | line4 -> 1s
+
+// 4s
