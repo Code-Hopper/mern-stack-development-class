4ea0eb2a8d852cd980019d753060ad2e1ed20ede
“code-hopper” <ameykhondekar01@gmail.com>
2026-06-29 09:02:10 +0530

    update

--- lessons/js/dom/new.js
+++ lessons/js/dom/new.js
@@ -1,72 +1,78 @@
-let target = document.getElementById("element1")
+// let target = document.getElementById("element1")
 
-console.log(target)
-console.log(target.innerHTML)
-console.log(target.innerText)
+// console.log(target)
+// console.log(target.innerHTML)
 // console.log(target.innerText)
+// // console.log(target.innerText)
 
-target.addEventListener("click", (e) => {
-    alert("clicked !")
-})
-
-// click, dblclick, mouse event(mousein/out/enter/leave) contextmenu
-
-// form event
-
-// form, input 
-
-// let form = document.getElementById("form1")
-
-// console.log(form)
-
-// form.addEventListener("submit",(event)=>{
-//     event.preventDefault()
-//     console.log(event)
-//     console.log("form submmited !")
-//     console.log(event.target[0].value)
-//     console.log(event.target[1].value)
-//     console.log(event.target[2].value)
-//     // console.log(event.target[3].value)
+// target.addEventListener("click", (e) => {
+//     alert("clicked !")
 // })
 
-// http://127.0.0.1:5500/lessons/js/dom/new.html
+// // click, dblclick, mouse event(mousein/out/enter/leave) contextmenu
 
-// http://127.0.0.1:5500/lessons/js/dom/new.html
+// // form event
 
-// http://127.0.0.1:5500/lessons/js/dom/new.html?
+// // form, input 
 
-// query
+// // let form = document.getElementById("form1")
 
-// http://127.0.0.1:5500/lessons/js/dom/new.html?name=Amey+Anil+Khondekar&age=21&phone=09766696550
+// // console.log(form)
 
-let red = document.getElementById("redR")
-let green = document.getElementById("greenR")
-let blue = document.getElementById("blueR")
+// // form.addEventListener("submit",(event)=>{
+// //     event.preventDefault()
+// //     console.log(event)
+// //     console.log("form submmited !")
+// //     console.log(event.target[0].value)
+// //     console.log(event.target[1].value)
+// //     console.log(event.target[2].value)
+// //     // console.log(event.target[3].value)
+// // })
 
-let redValue = 0
-let greenValue = 0
-let blueValue = 0
+// // http://127.0.0.1:5500/lessons/js/dom/new.html
 
-red.addEventListener("change", (event) => {
-    console.log(event.target.value)
-    redValue = event.target.value
-    applyColor()
-})
+// // http://127.0.0.1:5500/lessons/js/dom/new.html
 
-green.addEventListener("change", (event) => {
-    console.log(event.target.value)
-    greenValue = event.target.value
-    applyColor()
-})
+// // http://127.0.0.1:5500/lessons/js/dom/new.html?
 
-blue.addEventListener("change", (event) => {
-    console.log(event.target.value)
-    blueValue = event.target.value
-    applyColor()
-})
+// // query
 
-function applyColor() {
-    document.getElementById("element2").style.background = `rgb(${redValue},${greenValue}, ${blueValue})`
-}
+// // http://127.0.0.1:5500/lessons/js/dom/new.html?name=Amey+Anil+Khondekar&age=21&phone=09766696550
 
-applyColor()
+// let red = document.getElementById("redR")
+// let green = document.getElementById("greenR")
+// let blue = document.getElementById("blueR")
+
+// let redValue = 0
+// let greenValue = 0
+// let blueValue = 0
+
+// red.addEventListener("change", (event) => {
+//     console.log(event.target.value)
+//     redValue = event.target.value
+//     applyColor()
+// })
+
+// green.addEventListener("change", (event) => {
+//     console.log(event.target.value)
+//     greenValue = event.target.value
+//     applyColor()
+// })
+
+// blue.addEventListener("change", (event) => {
+//     console.log(event.target.value)
+//     blueValue = event.target.value
+//     applyColor()
+// })
+
+// function applyColor() {
+//     document.getElementById("element2").style.background = `rgb(${redValue},${greenValue}, ${blueValue})`
+// }
+
+// applyColor()
+
+// window.addEventListener("resize",(e)=>{
+//     alert("event called !")
+// })
+
+console.log("kuch aur !")
