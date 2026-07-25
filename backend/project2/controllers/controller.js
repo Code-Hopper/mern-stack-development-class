ebf01e28f788a3b38381861ddb304407fee13322
“code-hopper” <ameykhondekar01@gmail.com>
2026-07-25 08:37:57 +0530

    update

---
+++ backend/project2/controllers/controller.js
@@ -0,0 +1,24 @@
+const welcome = (req, res) => {
+    // res.status(200).json({ message: "welcome to root route!" })
+    res.status(200).render("index")
+}
+
+const handelPostWelcome = (req, res) => {
+    console.log(req.body)
+    // res.status(200).render("index")
+    res.status(300).redirect("/api/")
+}
+
+const message = (req, res) => {
+    res.status(200).json({ message: "welcome to message route!" })
+}
+
+const learnMore = (req, res) => {
+    res.status(200).json({ message: "welcome to learn more route!" })
+}
+
+const handelAbout = (req, res) => {
+    res.status(200).render("about")
+}
+
+export { welcome, message, learnMore, handelAbout, handelPostWelcome }
